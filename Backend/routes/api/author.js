c7549d8f9aa43c25ed5bc5e966e2154955060a02
ArRamos01 <67347455+ArRamos01@users.noreply.github.com>
2020-07-28 19:46:58 -0400

    Add files via upload

---
+++ Backend/routes/api/author.js
@@ -0,0 +1,32 @@
+
+const router = require('express').Router();
+let Author = require('../models/author.model');
+
+router.route('/').get((req, res) => {
+    Author.find()
+    .then(author => res.json(author))
+    .catch(err => res.status(400).json('Error: ' + err));
+});
+
+router.route('/add').post((req, res) => {
+    const authorFirst = req.body.authorFirst;
+    const authorLast = req.body.authorLast;
+    const biography = req.body.biography;
+    const publisher = req.body.publisher;
+    
+    const newAuthor = new Author({
+      authorFirst,
+      authorLast,
+      biography,
+      publisher,
+    });
+  
+    newAuthor.save()
+    .then(() => res.json('Author added!'))
+    .catch(err => res.status(400).json('Error: ' + err));
+  });
+
+  
+  module.exports = router;
+
+
---
+++ Backend/routes/api/book.js
@@ -0,0 +1,42 @@
+
+const router = require('express').Router();
+let Book = require('../models/book.model');
+
+router.route('/').get((req, res) => {
+  Book.find()
+    .then(book => res.json(book))
+    .catch(err => res.status(400).json('Error: ' + err));
+});
+
+router.route('/add').post((req, res) => {
+    const title = req.body.title;
+    const author = req.body.author;
+    const isbn = req.body.isbn;
+    const description = req.body.description;
+    const publisher = req.body.publisher;
+    const genre = req.body.genre;
+    const yearPublished = req.body.yearPublished;
+    const price = req.body.price;
+    const copiesSold = req.body.copiesSold;
+  
+    const newBook = new Book({
+      title,
+      author,
+      isbn,
+      description,
+      publisher,
+      genre,
+      yearPublished,
+      price,
+      copiesSold,
+    });
+  
+    newBook.save()
+    .then(() => res.json('Book added!'))
+    .catch(err => res.status(400).json('Error: ' + err));
+  });
+
+  
+  module.exports = router;
+
+
